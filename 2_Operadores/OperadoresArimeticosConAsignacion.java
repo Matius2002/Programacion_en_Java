98143d96c7df97989fe5c1e4828722d6d80facbb
Mateo Alejandro Mora Hernández <88298085+Matius2002@users.noreply.github.com>
2022-10-23 13:21:15 -0500

    Add files via upload

---
+++ 2_Operadores/OperadoresArimeticosConAsignacion.java
@@ -0,0 +1,14 @@
+//Operadores arimeticos con asignación
+package operadoresarimeticosconasignacion;
+
+public class OperadoresArimeticosConAsignacion {
+
+    public static void main(String[] args) {
+        int numero = 10;
+        
+        numero += 5;//numero = numero + 5;
+        
+        System.out.println("El número es: ");
+    }
+    
+}
